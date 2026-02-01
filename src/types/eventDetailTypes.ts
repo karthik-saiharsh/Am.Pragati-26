65a91836d3296c91c854e3e25f2020d348e57edb
Thanus Kumaar A <thanuskumaara@gmail.com>
2026-02-01 18:35:28 +0530

    Formatting of type files

--- src/types/eventDetailTypes.ts
+++ src/types/eventDetailTypes.ts
@@ -1,19 +1,18 @@
-import type { EventDetails } from './eventTypes';
-import type { GroupRegistrationOutput } from './groupRegistration';
-import type { Organizer } from "./eventTypes";
+import type { EventDetails, Organizer } from "./eventTypes";
+import type { GroupRegistrationOutput } from "./groupRegistration";
 
 export interface EventDetailProps {
-  event: EventDetails;
-  onStarToggle?: () => void;
-  onRegister?: () => void;
-  onGroupRegister?: (data: GroupRegistrationOutput) => void;
-  onFeedback?: () => void;
-  isStarLoading?: boolean;
-  isRegisterLoading?: boolean;
-  isLoggedIn?: boolean;
+	event: EventDetails;
+	onStarToggle?: () => void;
+	onRegister?: () => void;
+	onGroupRegister?: (data: GroupRegistrationOutput) => void;
+	onFeedback?: () => void;
+	isStarLoading?: boolean;
+	isRegisterLoading?: boolean;
+	isLoggedIn?: boolean;
 }
 
 export interface EventOrganisersProps {
-  event: EventDetails;
-  onOrganizerClick?: (organizer: Organizer) => void;
+	event: EventDetails;
+	onOrganizerClick?: (organizer: Organizer) => void;
 }
--- src/types/ticketTypes.ts
+++ src/types/ticketTypes.ts
@@ -1,30 +1,30 @@
-import { Schedule } from './eventTypes';
+import type { Schedule } from "./eventTypes";
 
 export interface Ticket {
-  event_id: string;
-  event_name: string;
-  schedules: Schedule[];
-  is_group: boolean;
-  team_name?: string;
-  price: number;
-  event_type: string;
-  is_technical: boolean;
-  event_mode: string;
-  tags: string[] | null;
-  specialEvent?: string;
+	event_id: string;
+	event_name: string;
+	schedules: Schedule[];
+	is_group: boolean;
+	team_name?: string;
+	price: number;
+	event_type: string;
+	is_technical: boolean;
+	event_mode: string;
+	tags: string[] | null;
+	specialEvent?: string;
 }
 
 export interface TicketResponse {
-  solo_events: Ticket[];
-  team_events: Ticket[];
+	solo_events: Ticket[];
+	team_events: Ticket[];
 }
 
 export interface TicketListProps {
-  listOftickets: Ticket[];
-  userId: string;
+	listOftickets: Ticket[];
+	userId: string;
 }
 
 export interface TicketProps {
-  ticket: Ticket;
-  userId: string;
+	ticket: Ticket;
+	userId: string;
 }
