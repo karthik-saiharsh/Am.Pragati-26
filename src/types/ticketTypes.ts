d1ab841bef614f4579f777de0082dba6e0e51979
Thanus Kumaar A <thanuskumaara@gmail.com>
2026-02-01 18:33:32 +0530

    created types for integration

---
+++ src/types/ticketTypes.ts
@@ -0,0 +1,30 @@
+import { Schedule } from './eventTypes';
+
+export interface Ticket {
+  event_id: string;
+  event_name: string;
+  schedules: Schedule[];
+  is_group: boolean;
+  team_name?: string;
+  price: number;
+  event_type: string;
+  is_technical: boolean;
+  event_mode: string;
+  tags: string[] | null;
+  specialEvent?: string;
+}
+
+export interface TicketResponse {
+  solo_events: Ticket[];
+  team_events: Ticket[];
+}
+
+export interface TicketListProps {
+  listOftickets: Ticket[];
+  userId: string;
+}
+
+export interface TicketProps {
+  ticket: Ticket;
+  userId: string;
+}
